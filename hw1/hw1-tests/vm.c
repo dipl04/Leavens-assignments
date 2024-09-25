c191ffc6c1a7c65ebbe46cd8c1e8c0b84f6b370b
JacobLegler <jacoblegler35@gmail.com>
2024-09-25 12:11:57 -0400

    started vm.c.  Opens and reads BOF files. Added functionality for -p method.

---
+++ hw1/hw1-tests/vm.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "bof.h"
+#include "instruction.h"
+#include "utilities.h"
+
+void print_bof(BOFFILE bof)
+{
+    // read the bof header
+    BOFHeader header = bof_read_header(bof);
+
+    // find the number of instructions
+    word_type num_instructions = header.text_length;
+
+    // print the header information
+    printf("BOF File: %s\n", bof.filename);
+    printf("Starting address: %u\n", header.text_start_address);
+    printf("Number of Instructions %u\n", num_instructions);
+    printf("Instructions:\n");
+
+    // loop through the instructions and print them
+    for (word_type i = 0; i < num_instructions; i++)
+    {
+        // read the instruction
+        bin_instr_t instruction = instruction_read(bof);
+
+        // convert it to assembly
+        const char *assembly_instruction = instruction_assembly_form(header.text_start_address + i, instruction);
+
+        // print the instruction
+        printf("%u: %s\n", i + header.text_start_address, assembly_instruction);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    // if argc < 2 throw an error and provide usage instructions
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: %s [-p] <bof_file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int print_mode = 0; // flag for if we are using -p option
+    char *bof_file;
+
+    // check for -p in the command line arguments
+    if (argc == 3 && strcmp(argv[1], "-p") == 0)
+    {
+        print_mode = 1;
+        bof_file = argv[2];
+    }
+    else
+    {
+        bof_file = argv[1];
+    }
+
+    // open and read the bof file
+    BOFFILE bof = bof_read_open(bof_file);
+
+    // check which mode we are in
+    if (print_mode)
+    {
+        // print the bof file
+        print_bof(bof);
+    }
+    else
+    {
+        // execute the program
+    }
+
+    return 0;
+}
